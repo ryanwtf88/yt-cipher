6bd92a5d1156e639d2c55598e9e19a71ec4a2e47
RY4N <ryanwtf88@gmail.com>
2025-10-27 20:59:32 +0600

    Update __init__.py

--- ejs/yt_dlp_ejs/yt/solver/__init__.py
+++ ejs/yt_dlp_ejs/yt/solver/__init__.py
@@ -2,15 +2,20 @@ import importlib.resources
 
 import yt_dlp_ejs.yt.solver
 
+
 def core() -> str:
     """
     Read the contents of the JavaScript core solver bundle as string.
     """
-    return importlib.resources.read_text(yt_dlp_ejs.yt.solver, "core.min.js")
+    return (importlib.resources.files(yt_dlp_ejs.yt.solver) / "core.min.js").read_text(
+        encoding="utf-8"
+    )
 
 
 def lib() -> str:
     """
     Read the contents of the JavaScript library solver bundle as string.
     """
-    return importlib.resources.read_text(yt_dlp_ejs.yt.solver, "lib.min.js")
+    return (importlib.resources.files(yt_dlp_ejs.yt.solver) / "lib.min.js").read_text(
+        encoding="utf-8"
+    )
